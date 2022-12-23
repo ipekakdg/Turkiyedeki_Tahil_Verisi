30dec62e3f9cfebdd45460fe5bd663000a15a24c
ipeka <ipeka@DESKTOP-1RI3SA7>
2022-12-23 14:08:45 +0300

    Proje dosyası ekle.

---
+++ Turkiyedeki_Tahil_Verisi/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Turkiyedeki_Tahil_Verisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.button1.Location = new System.Drawing.Point(12, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Türetim Verileri";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.SaddleBrown;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(464, 74);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(271, 360);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.button2.Location = new System.Drawing.Point(155, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 57);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Üretim Verileri";
+            this.button2.UseMnemonic = false;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.button3.Location = new System.Drawing.Point(302, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 57);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Stok Verileri";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.button4.Location = new System.Drawing.Point(14, 175);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 57);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Tahıl Verileri";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.button5.Location = new System.Drawing.Point(157, 175);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 57);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Tahılların Kullanıldığı Yerler";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.button6.Location = new System.Drawing.Point(304, 175);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(125, 57);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Taban Alımı";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.button7.Location = new System.Drawing.Point(14, 277);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(125, 57);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "İthalat Verileri";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button8.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button8.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.button8.Location = new System.Drawing.Point(157, 277);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(125, 57);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "İhracat Verileri";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button9.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button9.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.button9.Location = new System.Drawing.Point(304, 277);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(125, 57);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Yıl Bilgileri";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button10.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button10.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.button10.Location = new System.Drawing.Point(82, 377);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(125, 57);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Bağlantılı Şehirler";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button11.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button11.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.button11.Location = new System.Drawing.Point(248, 377);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(125, 57);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "Bağlantılı Ülkeler";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.label1.Location = new System.Drawing.Point(295, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 39);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "TÜRKİYEDE TAHIL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = global::Turkiyedeki_Tahil_Verisi.Properties.Resources.tahilanasayfa;
+            this.ClientSize = new System.Drawing.Size(773, 484);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Turkiyedeki_Tahil_Verisi/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Turkiyedeki_Tahil_Verisi
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter da;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void tahilVerileri()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-1RI3SA7\\SQLEXPRESS;Initial Catalog=Turkiyede_Tahil;Integrated Security=True");
+           
+            baglanti.Open();
+            da = new SqlDataAdapter("Select * From Tahillar", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        void tuketimVerileri()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-1RI3SA7\\SQLEXPRESS;Initial Catalog=Turkiyede_Tahil;Integrated Security=True");
+
+            baglanti.Open();
+            da = new SqlDataAdapter("Select * From Tuketim", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        void uretimVerileri()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-1RI3SA7\\SQLEXPRESS;Initial Catalog=Turkiyede_Tahil;Integrated Security=True");
+
+            baglanti.Open();
+            da = new SqlDataAdapter("Select * From Uretim", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        void stokVerileri()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-1RI3SA7\\SQLEXPRESS;Initial Catalog=Turkiyede_Tahil;Integrated Security=True");
+
+            baglanti.Open();
+            da = new SqlDataAdapter("Select * From Stok", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        void tahilkullanimalani()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-1RI3SA7\\SQLEXPRESS;Initial Catalog=Turkiyede_Tahil;Integrated Security=True");
+
+            baglanti.Open();
+            da = new SqlDataAdapter("Select * From TahilinKullanimYeri", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        void encokuretilenyer()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-1RI3SA7\\SQLEXPRESS;Initial Catalog=Turkiyede_Tahil;Integrated Security=True");
+
+            baglanti.Open();
+            da = new SqlDataAdapter("Select * From EnÇokUretilenYer", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        void ithalat()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-1RI3SA7\\SQLEXPRESS;Initial Catalog=Turkiyede_Tahil;Integrated Security=True");
+
+            baglanti.Open();
+            da = new SqlDataAdapter("Select * From İthalat", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        void ihracat()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-1RI3SA7\\SQLEXPRESS;Initial Catalog=Turkiyede_Tahil;Integrated Security=True");
+
+            baglanti.Open();
+            da = new SqlDataAdapter("Select * From Ihracat", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        void yil()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-1RI3SA7\\SQLEXPRESS;Initial Catalog=Turkiyede_Tahil;Integrated Security=True");
+
+            baglanti.Open();
+            da = new SqlDataAdapter("Select * From Yillar", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        void sehir()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-1RI3SA7\\SQLEXPRESS;Initial Catalog=Turkiyede_Tahil;Integrated Security=True");
+
+            baglanti.Open();
+            da = new SqlDataAdapter("Select * From EnÇokUretilenYer", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        void ulke()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-1RI3SA7\\SQLEXPRESS;Initial Catalog=Turkiyede_Tahil;Integrated Security=True");
+
+            baglanti.Open();
+            da = new SqlDataAdapter("Select * From Ulkeler", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        void tabanalimi()
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-1RI3SA7\\SQLEXPRESS;Initial Catalog=Turkiyede_Tahil;Integrated Security=True");
+
+            baglanti.Open();
+            da = new SqlDataAdapter("Select * From TabanAlımı", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tuketimVerileri();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            tahilVerileri();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            uretimVerileri();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stokVerileri();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            tahilkullanimalani();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            tabanalimi();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ithalat();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ihracat();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            yil();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            sehir();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            ulke();
+        }
+    }
+}
